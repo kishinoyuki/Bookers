6edfe3085212cda09cf715c377dd3c98b5e2c20c
kishinoyuki <171491104+kishinoyuki@users.noreply.github.com>
2024-06-25 14:16:20 +0000

    [doing] レイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-
   def create
     book = Book.new(book_params)
     book.save
@@ -10,6 +6,7 @@ class BooksController < ApplicationController
   end
   def index
     @books = Book.all
+    @book =Book.new
   end
 
   def show
