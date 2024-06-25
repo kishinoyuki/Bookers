e1d119c874dfd31e2a04ab402b00a3381f4736b0
kishinoyuki <171491104+kishinoyuki@users.noreply.github.com>
2024-06-25 12:24:04 +0000

    [finish] 削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,11 +15,22 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
-    
+
   def edit
+    @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id), method: :patch
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, method: :get
+  end
   private
   def book_params
     params.require(:book).permit(:title, :body)
