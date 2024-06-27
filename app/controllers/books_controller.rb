bba076291dd3b5f445c3bb1d6609bcced02efc5a
kishinoyuki <171491104+kishinoyuki@users.noreply.github.com>
2024-06-27 03:33:19 +0000

    [finish] フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
     else
     @books = Book.all
@@ -22,14 +23,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), method: :patch
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id), method: :patch
+    else
+    render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path, method: :get
   end
   private
